0fa8c991921d7915a3637c38e2508eed2a0660c2
JiaFu <962255329@qq.com>
2018-10-10 10:53:37 +0800

    登录、注册完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="login" style="height: 100%; background-color: #fff;">
+  <div class="login">
+    <!--header-->
+    <x-header :left-options="{showBack: false}">登 录</x-header>
     <!--logo-->
     <div class="login_logo">
       <img src="../../assets/logo.png" />
     </div>
-    <div>
+    <!--input-->
+    <div class="login_input">
       <x-input placeholder="请输入登录账号">
         <i slot="label" class="icon icon-yinhangqia1"></i>
       </x-input>
@@ -16,9 +19,14 @@
         <img slot="right-full-height" src="https://ws1.sinaimg.cn/large/663d3650gy1fq684go3glj203m01hmwy.jpg">
       </x-input>
     </div>
+    <!--按钮-->
     <box gap="10px 10px">
-      <x-button type="primary">primary</x-button>
+      <x-button class="but">登 录</x-button>
     </box>
+    <div class="login_features">
+      <router-link to="/registered">注册</router-link>
+      <router-link to="">忘记密码</router-link>
+    </div>
   </div>
 </template>
 
@@ -29,10 +37,32 @@ export default {
 </script>
 
 <style scoped lang="less">
-  .login_logo {
-    img {
-      display: block;
-      margin: 0 auto;
+  .login {
+    height: 100%;
+    background-color: #fff;
+    .login_logo {
+      img {
+        display: block;
+        margin: 0 auto;
+      }
+    }
+    .login_input {
+      padding-right: 10px;
+      i {
+        margin-right: 10px;
+      }
+    }
+    .login_features {
+      padding: 0 20px;
+      display: flex;
+      justify-content: space-between;
+      a {
+        color: @tabbar-text-active-color;
+      }
+    }
+    .but {
+      background-color: @but;
+      color: #fff;
     }
   }
 </style>
--- src/views/registered/registered.vue
+++ src/views/registered/registered.vue
@@ -1,24 +1,36 @@
 <template>
-  <div class="login" style="height: 100%; background-color: #fff;">
+  <div class="registered">
+    <!--header-->
+    <x-header :left-options="{showBack: false}">注 册</x-header>
     <!--logo-->
-    <div class="login_logo">
+    <div class="registered_logo">
       <img src="../../assets/logo.png" />
     </div>
-    <div>
-      <x-input placeholder="请输入登录账号">
+    <!--input-->
+    <div class="registered_input">
+      <x-input placeholder="请输入手机号码" class="weui-vcode">
+        <i slot="label" class="icon icon-yinhangqia1"></i>
+        <x-button slot="right" type="primary" mini class="but">发送验证码</x-button>
+      </x-input>
+      <x-input placeholder="请输入手机验证码">
         <i slot="label" class="icon icon-yinhangqia1"></i>
       </x-input>
       <x-input placeholder="请输入登录密码">
         <i slot="label" class="icon icon-yinhangqia1"></i>
       </x-input>
-      <x-input placeholder="请输入验证码">
+      <x-input placeholder="请再次输入登录密码">
         <i slot="label" class="icon icon-yinhangqia1"></i>
-        <img slot="right-full-height" src="https://ws1.sinaimg.cn/large/663d3650gy1fq684go3glj203m01hmwy.jpg">
+      </x-input>
+      <x-input placeholder="请输入邀请码">
+        <i slot="label" class="icon icon-yinhangqia1"></i>
       </x-input>
     </div>
     <box gap="10px 10px">
-      <x-button type="primary">primary</x-button>
+      <x-button type="primary" class="but">注 册</x-button>
     </box>
+    <div class="registered_features">
+      <router-link to="/login">登 录</router-link>
+    </div>
   </div>
 </template>
 
@@ -28,11 +40,31 @@ export default {
 }
 </script>
 
-<style scoped>
-  .login_logo {
-  img {
-    display: block;
-    margin: 0 auto;
-  }
+<style scoped lang="less">
+  .registered {
+    height: 100%;
+    background-color: #fff;
+    .registered_logo {
+      img {
+        display: block;
+        margin: 0 auto;
+      }
+    }
+    .registered_input {
+      padding-right: 10px;
+      i {
+        margin-right: 10px;
+      }
+    }
+    .but {
+      background-color: @but;
+      color: #fff;
+    }
+    .registered_features {
+      text-align: center;
+      a {
+        color: @tabbar-text-active-color;
+      }
+    }
   }
 </style>
